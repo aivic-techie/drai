092c9a592bba92b0a80729a9e792aed04d25c958
VictorAnthony506 <victor0anthony@gmail.com>
2023-11-24 21:39:54 +0100

    slash

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class UserMessage(BaseModel):
 
 # Initial system message
 messages = [
-    {"role": "system", "content": "You are a medical assistant. Respond in a friendly tone with concise answers. Ask relevant follow-up questions. In severe cases, advise the user to go to the hospital immediately."},
+    {"role": "system", "content": "You are a medical doctor. Respond in a friendly tone with concise answers. Ask relevant follow-up questions. In severe cases, advise the user to go to the hospital immediately."},
 ]
 
 @app.get("/")
@@ -32,20 +32,19 @@ async def root():
 # POST endpoint to receive user messages
 @app.post("/chat/")
 async def chat(user_message: UserMessage):
-    print({"reply": "Dr. AI: Goodbye! Stay healthy."})
     global messages
     message_content = user_message.message.strip().lower()
 
     # Handling special commands (exit, clear, help, info)
-    if message_content == "exit":
-        return {"reply": "Dr. AI: Goodbye! Stay healthy."}
-    elif message_content == "clear":
+    if message_content == "/exit":
+        return {"reply": "Dr. Anne: Goodbye! Stay healthy."}
+    elif message_content == "/clear":
         messages = [{"role": "system", "content": "You are a medical assistant capable of understanding symptoms."}]
-        return {"reply": "Dr. AI: Conversation cleared. Please describe your symptoms."}
-    elif message_content == "help":
-        return {"reply": "Dr. AI: You can describe your symptoms or ask health-related questions. Type 'clear' to restart our conversation or 'exit' to end it."}
-    elif message_content == "info":
-        return {"reply": "Dr. AI: I'm an AI-powered assistant trained to provide preliminary medical advice. Remember to consult a healthcare professional for accurate diagnosis."}
+        return {"reply": "Dr. Anne: Conversation cleared. Please describe your symptoms."}
+    elif message_content == "/help":
+        return {"reply": "Dr. Anne: You can describe your symptoms or ask health-related questions. Type '/clear' to restart our conversation or '/exit' to end it."}
+    elif message_content == "/info":
+        return {"reply": "Dr. Anne: I'm an AI-powered assistant trained to provide preliminary medical advice. Remember to consult a healthcare professional for accurate diagnosis."}
     else:
         # Add the user message to the conversation history
         messages.append({"role": "user", "content": message_content})
